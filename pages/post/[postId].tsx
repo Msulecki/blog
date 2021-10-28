dfac4520c737d095a3025cb56805041c58f93569
Mateusz Sułecki <mat.sulecki@gmail.com>
2021-10-28 06:20:18 +0200

    get title and description from article overview title

--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -1,20 +1,25 @@
 import { useRouter } from 'next/router';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import TopSection from '../../components/TopSection';
-import Articles from '../../components/Articles';
-import Footer from '../../components/Footer';
+import TopSection from '@components/TopSection';
+import Articles from '@components/Articles';
+import Footer from '@components/Footer';
+import articlesOverview from '@articles/articlesOverview.json';
 import styles from '../index.module.scss';
 
 const Post: NextPage = () => {
   const router = useRouter();
   const { postId } = router.query;
 
+  const title = articlesOverview?.filter(
+    (article) => article.slug === postId
+  )?.[0]?.title;
+
   return (
     <>
       <Head>
-        <title>Matt Sulecki - overview</title>
-        <meta name='description' content='Matt Sulecki - personal blog' />
+        <title>{title}</title>
+        <meta name='description' content={title} />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main className={styles['app']}>
