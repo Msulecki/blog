8a9b322b47183cabd35798fe6dbe525bebac85e4
Mateusz Sułecki <mat.sulecki@gmail.com>
2021-11-01 20:31:55 +0100

    add comment to inline script

--- public/script/loadThemeToggler.js
+++ public/script/loadThemeToggler.js
@@ -1,3 +1,8 @@
-const themeLoader = `!function(){function e(e){window.__APP_THEME=e,t=e,document.body.className=e,window.__onAfterThemeChange(e)}var t;window.__onAfterThemeChange=function(){};try{t=localStorage.getItem("theme")}catch(e){}window.__setTheme=function(t){e(t);try{localStorage.setItem("theme",t)}catch(e){}};var n=window.matchMedia("(prefers-color-scheme: dark)");n.addEventListener("onload",function(e){window.__setTheme(e.matches?"dark":"light")}),e(t||(n.matches?"dark":"light"))}();`;
+const comment =
+  '// theme changer, nothing scary \n// https://www.joshwcomeau.com/react/dark-mode\n';
+
+const themeLoader =
+  comment +
+  `!function(){function e(e){window.__APP_THEME=e,t=e,document.body.className=e,window.__onAfterThemeChange(e)}var t;window.__onAfterThemeChange=function(){};try{t=localStorage.getItem("theme")}catch(e){}window.__setTheme=function(t){e(t);try{localStorage.setItem("theme",t)}catch(e){}};var n=window.matchMedia("(prefers-color-scheme: dark)");n.addEventListener("onload",function(e){window.__setTheme(e.matches?"dark":"light")}),e(t||(n.matches?"dark":"light"))}();`;
 
 export default themeLoader;
