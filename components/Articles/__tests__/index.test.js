8ee592fb8f2ea2e2ae860712842e8d9993d9a03f
Mateusz Sułecki <mat.sulecki@gmail.com>
2022-02-12 20:51:30 +0100

    refactor tests

--- components/Articles/__tests__/index.test.js
+++ components/Articles/__tests__/index.test.js
@@ -1,42 +1,17 @@
-import articleComponents from '@articles/articleComponents';
 import articlesOverview from '@articles/articlesOverview.json';
-import { render, screen, waitFor } from '@testing-library/react';
-import Articles from '@components/Articles';
-
-const componentsSlugs = Object.keys(articleComponents);
-const articlesSlugs = articlesOverview.map((item) => item.slug);
+import convertPathToModuleName from '@articles/convertPathToModuleName';
+import * as allArticles from '@articles/index';
 
 describe('Articles', () => {
-  it('matches all slugs from config with articleComponents', () => {
-    expect(componentsSlugs).toHaveLength(articlesSlugs.length);
-
-    const isSlugsTheSame = componentsSlugs.every((key) => {
-      if (articlesSlugs.includes(key)) {
-        return true;
-      } else {
-        console.log('\x1b[31m%s\x1b[0m', `Slug not found: ${key}`);
-        return false;
-      }
-    });
-
-    expect(isSlugsTheSame).toBeTruthy();
+  it('matches articles overview corresponds to articles', () => {
+    expect(articlesOverview.length).toEqual(Object.keys(allArticles).length);
   });
 
-  it('renders page with correct post header and date', async () => {
-    const { rerender } = render(<Articles />);
-
-    for (const article of articlesOverview) {
-      const { slug, date, title } = article;
-
-      rerender(<Articles slug={slug} />);
-
-      await waitFor(() => {
-        const postDate = screen.getByText(date);
-        const postTile = screen.getByText(title);
-
-        expect(postDate).toBeInTheDocument();
-        expect(postTile).toBeInTheDocument();
-      });
-    }
+  it('converts slugs from overview to components', () => {
+    articlesOverview.forEach((article) => {
+      expect(
+        Object.keys(allArticles).includes(convertPathToModuleName(article.slug))
+      );
+    });
   });
 });
